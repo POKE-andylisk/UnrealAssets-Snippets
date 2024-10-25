0942470816f607552295fa66884217048a80ec91
andylisk <33792719+POKE-andylisk@users.noreply.github.com>
2024-10-24 19:14:33 -0700

    Update and rename 1_Hello_World_PyUnreal.py to Python_Snippets/1_Hello_World_PyUnreal.py

---
+++ Python_Snippets/1_Hello_World_PyUnreal.py
@@ -0,0 +1,8 @@
+# A basic hello world snippet, prints a log, a warning, and an error
+
+# exectue this file from the editor using Tools > Exceute python script in the top menu
+import unreal
+import os
+unreal.log("Hello World from 2024!")
+unreal.log_warning("A warning to the world!")
+unreal.log_error("An error to the world!")
